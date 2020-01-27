bb925948612744a51a82911fae3dd22935986a6b
vladd-bit <vlad.a.dinu@gmail.com>
2020-01-27 15:44:48 +0000

    Added GC settings and session cookie lifetime to prevent indefinite user sessions.

--- public/index.php
+++ public/index.php
@@ -37,7 +37,12 @@ try
         ini_set('session.save_path', dirname($_SERVER['DOCUMENT_ROOT']).\Application\Config\WebConfig::SESSION_STORAGE_PATH);
     }
 
+
+    /* GARBAGE COLLECTION SETTINGS */
+
+    ini_set('session.gc_maxlifetime', 3600);
     ini_set('session.gc_probability', 1);
+    session_set_cookie_params(3600);
 
     session_start();
 
