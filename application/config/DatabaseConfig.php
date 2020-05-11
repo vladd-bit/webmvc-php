016ed54ad1fd4d045c128aa119a2988703f0008a
vladd-bit <vlad.a.dinu@gmail.com>
2020-05-11 15:33:27 +0100

    Added DatabaseConfig.php file.

---
+++ application/config/DatabaseConfig.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Application\Config;
+
+class DatabaseConfig
+{
+    const DB_HOST = '127.0.0.1';
+
+    const DB_NAME = '';
+
+    const DB_USER = '';
+
+    const DB_PASSWORD = '';
+
+    const DB_ENGINE = 'pgsql';
+
+    const DB_PORT = '5432';
+
+    const DB_SQLITE_PATH = '';
+}
